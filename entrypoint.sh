b52a68e906e9ceb3a405064760247a64ba23e1ca
Rafael Calleja <rafaelcalleja@gmail.com>
2022-12-20 12:00:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,7 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
+pwd
+ls -la
+
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
